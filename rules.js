d55e439daf838fa3e2469601865e74e1109c01a1
ikbalhossain6081 <ikbalhossain6081@gmail.com>
2025-01-06 13:17:48 +0600

    some rules are written

---
+++ rules.js
@@ -0,0 +1,18 @@
+// variable defining rules 
+
+// 1. keyword cannot be used as variable name 
+
+var false = 34;
+// or 
+var var = 334:
+
+
+// 2. no space or gap in variable name is allowed 
+var is happy = false ;
+
+
+// 3. no quotation is allowed as variable name 
+
+var "address" = "anything" 
+
+4. 
---
+++ type.js
@@ -0,0 +1,16 @@
+// string type  and numeric type variable
+
+var name = "mahidul kabir";
+var address = 'lakshmipur, chittagong';
+var age = 27;
+console.log(name, age)
+
+// boolean type variable 
+var AreYouRich = true ;
+var passed = false;
+
+console.log(address, age, passed)
+
+
+// determination of variable type 
+console.log(typeof address)
