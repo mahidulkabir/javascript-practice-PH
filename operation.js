54a9a2b454f58affec912ed5d497c61afe2a216b
ikbalhossain6081 <ikbalhossain6081@gmail.com>
2025-01-06 22:50:16 +0600

    learned basic operation

--- operation.js
+++ operation.js
@@ -8,10 +8,16 @@ console.log(total)
 var a = 23;
 var b = 4 ;
 var total2 = a/b; //it means division
-var total3 = a%b; // it means finding the reminder
+var total3 = a%b; // it means finding the reminder or modulus 
 console.log(total2)
 console.log(total3)
 
 var finalPrice = total + total2 + total3 ;
 
 console.log (finalPrice)
+
+// ANOTHER THING
+// THIS IS EXPONENTIAL OR POWER REPRESENTATION ON JS
+
+var power = 5**2;
+console.log(power)
---
+++ shortcut.js
@@ -0,0 +1,10 @@
+var a = 34;
+a+=30;
+console.log(a)
+
+// this is the method of adding to the same variable 
+// same goes for subtraction and multiplication etc.
+
+var b = 30;
+b/=5;
+console.log(b)
